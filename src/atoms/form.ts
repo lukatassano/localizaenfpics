e5251b3969dea96f913de8a6383ab51e80069c17
Luka Avila <luka.avila@syonet.com>
2024-07-28 20:18:42 -0300

    test1

--- src/atoms/form.ts
+++
@@ -1,5 +0,0 @@
-import { atom } from "jotai";
-import { PersonalDataFormType } from "../types/form";
-
-export const personalDataFormAtom = atom<PersonalDataFormType | undefined>()
-
--- src/atoms/nurse.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from "jotai";
-import { atomWithStorage } from "jotai/utils";
-import { LatLng } from "leaflet";
-import { CompleteFormType } from "../types/form";
-
-export const nursesAtom = atomWithStorage<CompleteFormType[]>("nurses", [])
-export const filteredNursesAtom = atom<CompleteFormType[]>([])
-export const selectedNurseAtom = atom<CompleteFormType | undefined>(undefined);
-export const selectedCoordsAtom = atom<LatLng | undefined>(undefined);
