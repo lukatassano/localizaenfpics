71d6a715c2545e0693b2b858528a96249c5e1f1c
Luka Avila <luka.avila@syonet.com>
2024-08-26 21:14:44 -0300

    wip2: backend

--- backend/backend/controllers/NurseController.php
+++ backend/backend/controllers/NurseController.php
@@ -113,49 +113,114 @@ class NurseController {
         $data = json_decode(file_get_contents('php://input'), true);
         $cpf = $conn->real_escape_string($data['cpf']);
     
-        // Verificar se o enfermeiro já existe, usando CAST para converter o campo specialties em CHAR
-        $sql = "SELECT id, uuid, name, cpf, phone, email, instagram, facebook, twitter, linkedin, birthday, gender, coren, 
-                CAST(specialties AS CHAR) AS specialties, lgpd, zipCode, type, street, district, complement, city, state, country, 
-                number, latitude, longitude, exactLocation, created_at 
-                FROM nurse WHERE cpf = '$cpf'";
+        // Verificar se o enfermeiro já existe e obter o ID se existir
+        $sql = "SELECT id, COUNT(*) as count FROM nurse WHERE cpf = '$cpf'";
         $result = $conn->query($sql);
     
         if ($result === false) {
             die("Error executing SQL: " . $conn->error);
         }
     
-        // Encode specialties para uma string JSON
-        $specialties = $conn->real_escape_string(json_encode($data['specialties']));
+        // Obter o número de registros encontrados com o mesmo CPF
+        $row = $result->fetch_assoc();
+        $recordCount = $row['count'];
+        $existingId = isset($row['id']) ? $row['id'] : null;
     
-        if ($result->num_rows > 0) {
+        // Verifique e sanitize todos os dados
+        $uuid = isset($data['uuid']) ? $conn->real_escape_string($data['uuid']) : '';
+        $name = isset($data['name']) ? $conn->real_escape_string($data['name']) : '';
+        $phone = isset($data['phone']) ? $conn->real_escape_string($data['phone']) : '';
+        $email = isset($data['email']) ? $conn->real_escape_string($data['email']) : '';
+        $instagram = isset($data['instagram']) ? $conn->real_escape_string($data['instagram']) : '';
+        $facebook = isset($data['facebook']) ? $conn->real_escape_string($data['facebook']) : '';
+        $twitter = isset($data['twitter']) ? $conn->real_escape_string($data['twitter']) : '';
+        $linkedin = isset($data['linkedin']) ? $conn->real_escape_string($data['linkedin']) : '';
+        $birthday = isset($data['birthday']) ? $conn->real_escape_string($data['birthday']) : '';
+        $gender = isset($data['gender']) ? $conn->real_escape_string($data['gender']) : '';
+        $coren = isset($data['coren']) ? $conn->real_escape_string($data['coren']) : '';
+        $specialties = isset($data['specialties']) ? $conn->real_escape_string(json_encode($data['specialties'])) : '';
+        $lgpd = isset($data['lgpd']) ? $conn->real_escape_string($data['lgpd']) : 0;
+    
+        // Verificar e sanitize todos os campos de endereço diretamente
+        $zipCode = isset($data['zipCode']) ? $conn->real_escape_string($data['zipCode']) : '';
+        $type = isset($data['type']) ? $conn->real_escape_string($data['type']) : '';
+        $street = isset($data['street']) ? $conn->real_escape_string($data['street']) : '';
+        $district = isset($data['district']) ? $conn->real_escape_string($data['district']) : '';
+        $complement = isset($data['complement']) ? $conn->real_escape_string($data['complement']) : '';
+        $city = isset($data['city']) ? $conn->real_escape_string($data['city']) : '';
+        $state = isset($data['state']) ? $conn->real_escape_string($data['state']) : '';
+        $country = isset($data['country']) ? $conn->real_escape_string($data['country']) : '';
+        $number = isset($data['number']) ? $conn->real_escape_string($data['number']) : '';
+        $latitude = isset($data['latitude']) ? $conn->real_escape_string($data['latitude']) : 'NULL';
+        $longitude = isset($data['longitude']) ? $conn->real_escape_string($data['longitude']) : 'NULL';
+        $exactLocation = isset($data['exactLocation']) ? $conn->real_escape_string($data['exactLocation']) : 0;
+    
+        if ($recordCount > 0 && $existingId !== null) {
             // Atualizar o registro existente
             $sql = "UPDATE nurse SET 
-                        uuid = '{$conn->real_escape_string($data['uuid'])}', 
-                        name = '{$conn->real_escape_string($data['name'])}', 
-                        phone = '{$conn->real_escape_string($data['phone'])}', 
-                        email = '{$conn->real_escape_string($data['email'])}', 
-                        instagram = '{$conn->real_escape_string($data['instagram'])}', 
-                        facebook = '{$conn->real_escape_string($data['facebook'])}', 
-                        twitter = '{$conn->real_escape_string($data['twitter'])}', 
-                        linkedin = '{$conn->real_escape_string($data['linkedin'])}', 
-                        birthday = '{$conn->real_escape_string($data['birthday'])}', 
-                        gender = '{$conn->real_escape_string($data['gender'])}', 
-                        coren = '{$conn->real_escape_string($data['coren'])}', 
+                        uuid = '$uuid', 
+                        name = '$name', 
+                        phone = '$phone', 
+                        email = '$email', 
+                        instagram = '$instagram', 
+                        facebook = '$facebook', 
+                        twitter = '$twitter', 
+                        linkedin = '$linkedin', 
+                        birthday = '$birthday', 
+                        gender = '$gender', 
+                        coren = '$coren', 
                         specialties = '$specialties', 
-                        lgpd = {$conn->real_escape_string($data['lgpd'])}, 
-                        zipCode = '{$conn->real_escape_string($data['address']['zipCode'])}', 
-                        type = '{$conn->real_escape_string($data['address']['type'])}', 
-                        street = '{$conn->real_escape_string($data['address']['street'])}', 
-                        district = '{$conn->real_escape_string($data['address']['district'])}', 
-                        complement = '{$conn->real_escape_string($data['address']['complement'])}', 
-                        city = '{$conn->real_escape_string($data['address']['city'])}', 
-                        state = '{$conn->real_escape_string($data['address']['state'])}', 
-                        country = '{$conn->real_escape_string($data['address']['country'])}', 
-                        number = '{$conn->real_escape_string($data['address']['number'])}', 
-                        latitude = {$conn->real_escape_string($data['address']['latitude'])}, 
-                        longitude = {$conn->real_escape_string($data['address']['longitude'])}, 
-                        exactLocation = {$conn->real_escape_string($data['address']['exactLocation'])} 
+                        lgpd = $lgpd, 
+                        zipCode = '$zipCode', 
+                        type = '$type', 
+                        street = '$street', 
+                        district = '$district', 
+                        complement = '$complement', 
+                        city = '$city', 
+                        state = '$state', 
+                        country = '$country', 
+                        number = '$number', 
+                        latitude = $latitude, 
+                        longitude = $longitude, 
+                        exactLocation = $exactLocation 
                     WHERE cpf = '$cpf'";
+    
+            if ($conn->query($sql) === TRUE) {
+                // Retornar o objeto atualizado com o ID
+                $updatedNurse = [
+                    "id" => $existingId,
+                    "uuid" => $uuid,
+                    "name" => $name,
+                    "cpf" => $cpf,
+                    "phone" => $phone,
+                    "email" => $email,
+                    "instagram" => $instagram,
+                    "facebook" => $facebook,
+                    "twitter" => $twitter,
+                    "linkedin" => $linkedin,
+                    "birthday" => $birthday,
+                    "gender" => $gender,
+                    "coren" => $coren,
+                    "specialties" => json_decode($specialties, true),
+                    "lgpd" => $lgpd,
+                    "zipCode" => $zipCode,
+                    "type" => $type,
+                    "street" => $street,
+                    "district" => $district,
+                    "complement" => $complement,
+                    "city" => $city,
+                    "state" => $state,
+                    "country" => $country,
+                    "number" => $number,
+                    "latitude" => $latitude !== 'NULL' ? (float)$latitude : null,
+                    "longitude" => $longitude !== 'NULL' ? (float)$longitude : null,
+                    "exactLocation" => $exactLocation
+                ];
+    
+                echo json_encode($updatedNurse);
+            } else {
+                echo json_encode(["message" => "Error updating nurse record: " . $conn->error]);
+            }
         } else {
             // Inserir um novo registro
             $sql = "INSERT INTO nurse (
@@ -163,42 +228,76 @@ class NurseController {
                         birthday, gender, coren, specialties, lgpd, zipCode, type, street, district, 
                         complement, city, state, country, number, latitude, longitude, exactLocation
                     ) VALUES (
-                        '{$conn->real_escape_string($data['uuid'])}', 
-                        '{$conn->real_escape_string($data['name'])}', 
+                        '$uuid', 
+                        '$name', 
                         '$cpf', 
-                        '{$conn->real_escape_string($data['phone'])}', 
-                        '{$conn->real_escape_string($data['email'])}', 
-                        '{$conn->real_escape_string($data['instagram'])}', 
-                        '{$conn->real_escape_string($data['facebook'])}', 
-                        '{$conn->real_escape_string($data['twitter'])}', 
-                        '{$conn->real_escape_string($data['linkedin'])}', 
-                        '{$conn->real_escape_string($data['birthday'])}', 
-                        '{$conn->real_escape_string($data['gender'])}', 
-                        '{$conn->real_escape_string($data['coren'])}', 
+                        '$phone', 
+                        '$email', 
+                        '$instagram', 
+                        '$facebook', 
+                        '$twitter', 
+                        '$linkedin', 
+                        '$birthday', 
+                        '$gender', 
+                        '$coren', 
                         '$specialties', 
-                        {$conn->real_escape_string($data['lgpd'])}, 
-                        '{$conn->real_escape_string($data['address']['zipCode'])}', 
-                        '{$conn->real_escape_string($data['address']['type'])}', 
-                        '{$conn->real_escape_string($data['address']['street'])}', 
-                        '{$conn->real_escape_string($data['address']['district'])}', 
-                        '{$conn->real_escape_string($data['address']['complement'])}', 
-                        '{$conn->real_escape_string($data['address']['city'])}', 
-                        '{$conn->real_escape_string($data['address']['state'])}', 
-                        '{$conn->real_escape_string($data['address']['country'])}', 
-                        '{$conn->real_escape_string($data['address']['number'])}', 
-                        {$conn->real_escape_string($data['address']['latitude'])}, 
-                        {$conn->real_escape_string($data['address']['longitude'])}, 
-                        {$conn->real_escape_string($data['address']['exactLocation'])}
+                        $lgpd, 
+                        '$zipCode', 
+                        '$type', 
+                        '$street', 
+                        '$district', 
+                        '$complement', 
+                        '$city', 
+                        '$state', 
+                        '$country', 
+                        '$number', 
+                        $latitude, 
+                        $longitude, 
+                        $exactLocation
                     )";
-        }
     
-        // Execute a consulta SQL
-        if ($conn->query($sql) === TRUE) {
-            echo json_encode(["message" => "Nurse record saved successfully"]);
-        } else {
-            echo json_encode(["message" => "Error saving nurse record: " . $conn->error]);
+            if ($conn->query($sql) === TRUE) {
+                // Obter o ID do registro recém-inserido
+                $insertedId = $conn->insert_id;
+    
+                // Construir o objeto de retorno diretamente dos dados
+                $savedNurse = [
+                    "id" => $insertedId,
+                    "uuid" => $uuid,
+                    "name" => $name,
+                    "cpf" => $cpf,
+                    "phone" => $phone,
+                    "email" => $email,
+                    "instagram" => $instagram,
+                    "facebook" => $facebook,
+                    "twitter" => $twitter,
+                    "linkedin" => $linkedin,
+                    "birthday" => $birthday,
+                    "gender" => $gender,
+                    "coren" => $coren,
+                    "specialties" => json_decode($specialties, true),
+                    "lgpd" => $lgpd,
+                    "zipCode" => $zipCode,
+                    "type" => $type,
+                    "street" => $street,
+                    "district" => $district,
+                    "complement" => $complement,
+                    "city" => $city,
+                    "state" => $state,
+                    "country" => $country,
+                    "number" => $number,
+                    "latitude" => $latitude !== 'NULL' ? (float)$latitude : null,
+                    "longitude" => $longitude !== 'NULL' ? (float)$longitude : null,
+                    "exactLocation" => $exactLocation
+                ];
+    
+                echo json_encode($savedNurse);
+            } else {
+                echo json_encode(["message" => "Error saving nurse record: " . $conn->error]);
+            }
         }
-    }    
+    }
+    
 
 }
 
